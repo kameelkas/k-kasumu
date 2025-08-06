a713c46cb8913c10db112f631cbaf4e39d989131
Kameel Kasumu <kameelkas@gmail.com>
2025-08-05 22:10:14 -0600

    feat: Hero section pt. 3

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,7 +4,7 @@ import DarkKK from '@/app/assets/dark-KK.jpeg';
 import LightKK from '@/app/assets/light-KK.jpeg';
 import { SiGithub, SiLinkedin } from 'react-icons/si';
 import { IoIosPaper } from 'react-icons/io';
-import { MdEmail } from "react-icons/md";
+import { MdEmail } from 'react-icons/md';
 import { useEffect, useState } from 'react';
 export default function Hero() {
   const [isDark, setIsDark] = useState(false);
@@ -12,23 +12,23 @@ export default function Hero() {
     {
       name: 'GitHub',
       url: 'https:://github.com/kameelkas',
-      icon: <SiGithub className="h-9 w-9 md:h-12 md:w-12" />
+      icon: <SiGithub size={36} />,
     },
     {
       name: 'LinkedIn',
       url: 'https://www.linkedin.com/in/ka-kasumu/',
-      icon: <SiLinkedin className="h-9 w-9 md:h-12 md:w-12" />
+      icon: <SiLinkedin size={36} />,
     },
     {
       name: 'Resume',
       url: '#',
-      icon: <IoIosPaper className="h-9 w-9 md:h-12 md:w-12" />
+      icon: <IoIosPaper size={36} />,
     },
     {
-        name: 'Email',
-        url: 'mailto:kameelkas@gmail.com',
-        icon: <MdEmail className="h-9 w-9 md:h-14 md:w-14" />
-    }
+      name: 'Email',
+      url: 'mailto:kameelkas@gmail.com',
+      icon: <MdEmail size={36} />,
+    },
   ];
 
   useEffect(() => {
@@ -58,14 +58,14 @@ export default function Hero() {
           <h1 className="text-4xl sm:text-5xl">Hey, I&apos;m</h1>
           <h1 className="text-6xl font-bold sm:text-8xl">Kameel!</h1>
           <h1 className="text-xl sm:text-2xl">Aspiring Software Engineer</h1>
-          <div className="mt-3 space-x-8">
+          <div className="mt-3 flex items-center justify-between space-x-6">
             {heroLinks.map((link) => (
               <a
                 key={link.name}
                 href={link.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline-flex items-center justify-center text-primary-light hover:text-accent-light dark:text-primary-dark dark:hover:text-accent-dark"
+                className="text-primary-light hover:text-accent-light dark:text-primary-dark dark:hover:text-accent-dark"
               >
                 {link.icon}
               </a>
