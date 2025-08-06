0e1268def48b6d460e2fc4991b6996a5d722abe6
Kameel Kasumu <kameelkas@gmail.com>
2025-08-06 00:35:58 -0600

    feat: add word to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       </h1>
       <div className="mx-auto mt-8 max-w-md md:max-w-4xl text-justify">
         <p className="mt-4 text-lg sm:text-xl md:text-2xl">
-          Hey! I&apos;m Kameel, a final year Software Engineering student at the
+          Hey! I&apos;m Kameel, a final year Software & Mechatronics Engineering student at the
           University of Calgary. I previously interned at Seequent as a Software
           Developer Intern, where I primarily worked on the UI/UX of a
           geotechnical analysis software. In my free time, I enjoy creating
