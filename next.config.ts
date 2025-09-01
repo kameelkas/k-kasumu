a0353101e493e4807e8227f3f856b6731a1152a7
Kameel Kasumu <kameelkas@gmail.com>
2025-09-01 13:47:53 -0600

    feat: Automate 'Last Updated' Date

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_BUILD_DATE:
+      process.env.VERCEL_GIT_COMMIT_AUTHOR_DATE || new Date().toISOString(),
+  },
 };
 
 export default nextConfig;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,18 @@ import { HiCode } from 'react-icons/hi';
 
 export default function Footer() {
   const year = new Date().getFullYear();
+  const buildDate = new Date(
+    process.env.NEXT_PUBLIC_BUILD_DATE || ""
+  ).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+  });
 
   return (
     <footer>
       <div className="flex flex-col items-center justify-center space-x-2 text-sm font-semibold text-gray-400 dark:text-gray-600">
         <span>© {year} Kameel Kasumu. All rights reserved.</span>
-        <span>Last updated: August 2025</span>
+        <span>Last updated: {buildDate}</span>
         <motion.span
           className="inline-block h-5 w-5 text-accent-light dark:text-accent-dark"
           animate={{ scale: [1, 1.3, 1] }}
